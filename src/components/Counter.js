e6f385e7456d73857d6337f54cc0c3ade9f99812
karaderushabh <61509850+karaderushabh@users.noreply.github.com>
2023-12-13 00:17:17 +0530

    branch2 updated

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -70,6 +70,7 @@ class Counter extends React.Component {
         <div align="center">
           <h1 className="p3">Counter App</h1>
           <h2 className="p1" data-testid="counter">
+            <h3>Counter Project</h3>
             Count : {this.state.counter}
           </h2>
           <button className="button button1" onClick={this.handleIncrement}>
